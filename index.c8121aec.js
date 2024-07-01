652865f06908b769a6e0a5cd2a09cc42190fae35
happytravels38 <happytravels38@gmail.com>
2024-07-01 14:29:35 -0400

    make build

---
+++ index.c8121aec.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var t=document.querySelector(".wall"),e=document.querySelector(".spider"),n=t.getBoundingClientRect(),o=e.getBoundingClientRect(),i=(n.width-o.width)/2,c=(n.height-o.height)/2;e.style.position="absolute",e.style.left="".concat(i,"px"),e.style.top="".concat(c,"px")});
+//# sourceMappingURL=index.c8121aec.js.map
